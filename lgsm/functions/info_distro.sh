92a9232643fd6a6a059716a5bb1502bc7f176834
Kai Weutzing <kai@weutzing.de>
2021-02-18 11:23:23 +0100

    added tmux list-panes as a fall back

--- lgsm/functions/info_distro.sh
+++ lgsm/functions/info_distro.sh
@@ -11,7 +11,12 @@ functionselfname="$(basename "$(readlink -f "${BASH_SOURCE[0]}")")"
 ### Game Server pid
 if [ "${status}" == "1" ]; then
 	gameserverpid=$(tmux list-sessions -F "#{session_name} #{pane_pid}" | grep "^${sessionname} " | awk '{print $NF}')
+		if [ "${gameserverpid}" ]
+		then
+			gameserverpid=$(tmux list-panes -F "#{session_name} #{pane_pid}" | grep "^${sessionname} " | awk '{print $NF}')
+		fi
 fi
+
 ### Distro information
 
 ## Distro
